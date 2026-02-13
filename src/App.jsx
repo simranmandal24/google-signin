fc0e9348b84cc0ba822f4aa601d43ec1df7d3da2
simranmandal24 <simranmandal24@navgurukul.org>
2026-02-13 12:11:11 +0530

    changed api

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import AuthCard from "./AuthCard";
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId="477480793924-42ha6uriv1u4fcosgrv2ugkvkvpvmdig.apps.googleusercontent.com
-">
+    <GoogleOAuthProvider clientId="477480793924-42ha6uriv1u4fcosgrv2ugkvkvpvmdig.apps.googleusercontent.com">
       <AuthCard />
     </GoogleOAuthProvider>
   );
